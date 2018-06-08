30bed0391c7a4734d6d5e311d96ed44bf03f14be
Diego Pacheco <diego.pacheco.it@gmail.com>
2018-06-08 00:54:08 -0700

    simple version of fetcher

--- github-fetcher/main.go
+++ github-fetcher/main.go
@@ -4,8 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
+	"strconv"
 	"time"
 )
 
@@ -13,36 +13,52 @@ type Repo struct {
 	Name string `json:"full_name"`
 }
 
-func extractRepos() []Repo {
-	url := "https://api.github.com/orgs/Netflix/repos"
+func extractRepos(page int) ([]Repo, error) {
+	url := "https://api.github.com/orgs/Netflix/repos?page=" + strconv.Itoa(page)
 	client := http.Client{
 		Timeout: time.Second * 2,
 	}
 
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("Error 1")
+		return nil, err
 	}
 	res, getErr := client.Do(req)
 	if getErr != nil {
-		log.Fatal(getErr)
+		fmt.Println("Error 2")
+		return nil, getErr
 	}
 	body, readErr := ioutil.ReadAll(res.Body)
 	if readErr != nil {
-		log.Fatal(readErr)
+		fmt.Println("Error 3")
+		return nil, readErr
 	}
 
 	repos := make([]Repo, 0)
 	jsonErr := json.Unmarshal(body, &repos)
 	if jsonErr != nil {
-		log.Fatal(jsonErr)
+		fmt.Println("Error 4")
+		return nil, jsonErr
 	}
-	return repos
+	return repos, nil
 }
 
 func main() {
-	repos := extractRepos()
-	for _, r := range repos {
-		fmt.Println(r.Name)
+
+	// todo: 1 - figure out how many pages until error
+	// todo: 2 - add all results to single list
+	// todo: 3 - persist result in FS
+	// todo: 4 - make diff with previous FS and now
+
+	repos, err := extractRepos(5)
+	if err == nil {
+		for _, r := range repos {
+			fmt.Println(r.Name)
+		}
+	} else {
+		fmt.Println("Error")
+		fmt.Println(err)
 	}
+
 }

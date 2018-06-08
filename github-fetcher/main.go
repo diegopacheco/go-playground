eafb82e2a2d8f05777b6072761f2371bcd6c0be7
Diego Pacheco <diego.pacheco.it@gmail.com>
2018-06-08 11:50:54 -0700

    improve code

--- github-fetcher/main.go
+++ github-fetcher/main.go
@@ -13,12 +13,11 @@ type Repo struct {
 	Name string `json:"full_name"`
 }
 
-func extractRepos(page int) ([]Repo, error) {
-	url := "https://api.github.com/orgs/Netflix/repos?page=" + strconv.Itoa(page)
+func extractRepos(page int, orgname string) ([]Repo, error) {
+	url := "https://api.github.com/orgs/" + orgname + "/repos?page=" + strconv.Itoa(page)
 	client := http.Client{
 		Timeout: time.Second * 2,
 	}
-
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		fmt.Println("Error 1")
@@ -34,7 +33,6 @@ func extractRepos(page int) ([]Repo, error) {
 		fmt.Println("Error 3")
 		return nil, readErr
 	}
-
 	repos := make([]Repo, 0)
 	jsonErr := json.Unmarshal(body, &repos)
 	if jsonErr != nil {
@@ -44,21 +42,28 @@ func extractRepos(page int) ([]Repo, error) {
 	return repos, nil
 }
 
-func main() {
+func getAllRepos(orgname string) []Repo {
+	var allRepos []Repo
+	pagination := 1
+	for true {
+		repos, _ := extractRepos(pagination, orgname)
+		if len(repos) == 0 {
+			break
+		}
+		for _, r := range repos {
+			allRepos = append(allRepos, r)
+		}
+		pagination = pagination + 1
+	}
+	return allRepos
+}
 
-	// todo: 1 - figure out how many pages until error
-	// todo: 2 - add all results to single list
+func main() {
 	// todo: 3 - persist result in FS
 	// todo: 4 - make diff with previous FS and now
-
-	repos, err := extractRepos(5)
-	if err == nil {
-		for _, r := range repos {
-			fmt.Println(r.Name)
-		}
-	} else {
-		fmt.Println("Error")
-		fmt.Println(err)
+	allRepos := getAllRepos("Netflix")
+	for _, o := range allRepos {
+		fmt.Println(o.Name)
 	}
 
 }

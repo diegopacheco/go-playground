c64698c7b6516a747732c7a1f0c36c2e45eb5ead
Diego Pacheco <diego.pacheco.it@gmail.com>
2018-06-08 19:57:47 -0700

    simple etcd3 client impl

---
+++ etcd-client-fun/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/coreos/etcd/clientv3"
+)
+
+func main() {
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"localhost:2379", "localhost:22379", "localhost:32379"},
+		DialTimeout: 5 * time.Second,
+	})
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Etcd Client connected")
+	}
+	cli.Close()
+}

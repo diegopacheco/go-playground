c83c9f8b5fe933005852b217039f3cea75db96a2
Diego Pacheco <diego.pacheco.it@gmail.com>
2018-07-12 22:01:13 -0700

    Recover from Panic + math exps

--- go-repl/main.go
+++ go-repl/main.go
@@ -6,16 +6,35 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"time"
+
+	"github.com/knetic/govaluate"
 )
 
 func printRepl() {
 	fmt.Print("go-repl> ")
 }
 
+func recoverExp(text string) {
+	if r := recover(); r != nil {
+		fmt.Println("go-repl> unknow command ", text)
+	}
+}
+
 func printInvalidCmd(text string) {
+	// We might have a panic here we so need DEFER + RECOVER
+	defer recoverExp(text)
+	// \n Will be ignored
 	t := strings.TrimSuffix(text, "\n")
 	if t != "" {
-		fmt.Println("go-repl> unknow command " + t)
+		expression, errExp := govaluate.NewEvaluableExpression(text)
+		result, errEval := expression.Evaluate(nil)
+		// Before we need to know if is not a Math expr
+		if errExp == nil && errEval == nil {
+			fmt.Println("go-repl>", result)
+		} else {
+			fmt.Println("go-repl> unknow command " + t)
+		}
 	}
 }
 
@@ -35,9 +54,11 @@ func help() {
 	fmt.Println("go-repl> Welcome to Go Repl! ")
 	fmt.Println("go-repl> Wrote by Diego Pacheco - 2018 ")
 	fmt.Println("go-repl> This Are the Avaliable commands: ")
-	fmt.Println("go-repl> help - Show you the Help")
-	fmt.Println("go-repl> cls  - Clear the Terminal Screen ")
-	fmt.Println("go-repl> exit - Exits the Go REPL ")
+	fmt.Println("go-repl> help   - Show you the Help")
+	fmt.Println("go-repl> cls    - Clear the Terminal Screen ")
+	fmt.Println("go-repl> exit   - Exits the Go REPL ")
+	fmt.Println("go-repl> 1 + 2  - Its possible todo Math expressions: true == true, 4 * 6 / 2, 2 > 1 ")
+	fmt.Println("go-repl> time   - Prints current date / time ")
 	fmt.Println("go-repl> ")
 }
 
@@ -47,10 +68,15 @@ func cls() {
 	cmd.Run()
 }
 
+func now() {
+	fmt.Println("go-repl> ", time.Now().Format(time.RFC850))
+}
+
 func main() {
 	commands := map[string]interface{}{
 		"help": help,
 		"cls":  cls,
+		"time": now,
 	}
 	reader := bufio.NewReader(os.Stdin)
 	help()

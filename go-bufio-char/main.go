9d02d27082d340431fcd93c54234a7de9de79f06
Diego Pacheco <diego.pacheco.it@gmail.com>
2018-07-17 22:34:20 -0700

    all arrows

--- go-bufio-char/main.go
+++ go-bufio-char/main.go
@@ -5,16 +5,21 @@ import (
 	"bytes"
 	"fmt"
 	"os"
-	"os/exec"
 	"strings"
 )
 
 func main() {
-	exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
-	exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
-	defer exec.Command("stty", "-F", "/dev/tty", "echo").Run()
 
+	// avoid buffer and avoid to print :(
+	/*
+		exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
+		exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
+		defer exec.Command("stty", "-F", "/dev/tty", "echo").Run()
+	*/
 	Up := []byte{27, 91, 65}
+	Down := []byte{27, 91, 66}
+	Right := []byte{27, 91, 67}
+	Left := []byte{27, 91, 68}
 
 	r := bufio.NewReader(os.Stdin)
 	strLine, _ := r.ReadString('\n')
@@ -23,10 +28,15 @@ func main() {
 
 	fmt.Printf("HERE Up Means: % x] \n", Up)
 	fmt.Printf("Byte String % x] ", bytes.NewBufferString(strLine).Bytes()[0:3])
-	fmt.Printf("Comp Up and Str: %t \n", bytes.Equal(bytes.NewBufferString(strLine).Bytes()[0:3], Up))
 
 	if bytes.Equal(bytes.NewBufferString(strLine).Bytes()[0:3], Up) == true {
 		fmt.Println("UP pressed")
+	} else if bytes.Equal(bytes.NewBufferString(strLine).Bytes()[0:3], Down) == true {
+		fmt.Println("Down pressed")
+	} else if bytes.Equal(bytes.NewBufferString(strLine).Bytes()[0:3], Left) == true {
+		fmt.Println("Left pressed")
+	} else if bytes.Equal(bytes.NewBufferString(strLine).Bytes()[0:3], Right) == true {
+		fmt.Println("Right pressed")
 	} else {
 		fmt.Print("Unkonw: ")
 		fmt.Println(strLine)

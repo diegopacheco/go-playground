2235b3b750acc60f1bfe5beb5e7d2f6bd0b7001c
Diego Pacheco <diego.pacheco.it@gmail.com>
2018-09-19 16:21:55 -0300

    chan go routine

---
+++ go-routines/chan-go-routine.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	c := make(chan string)
+	go count("cats", c)
+	/* Receiver */
+	for {
+		msg := <-c
+		fmt.Println(msg)
+	}
+}
+
+/* Sender */
+func count(msg string, c chan string) {
+	for i := 1; i <= 6; i++ {
+		c <- msg
+		time.Sleep(time.Millisecond * 500)
+	}
+	close(c)
+}

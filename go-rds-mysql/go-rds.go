a079f334e48658a630b1ff5cd58906b8028875e7
Diego Pacheco <diego.pacheco.it@gmail.com>
2019-02-07 11:14:04 -0200

    go RDS sample

---
+++ go-rds-mysql/go-rds.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "user:password@tcp(YOUR_CLUSTER_NAME.us-west-2.rds.amazonaws.com)/db")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT msg FROM db.Messages")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer rows.Close()
+
+	columns, err := rows.Columns()
+	if err != nil {
+		panic(err.Error())
+	}
+	values := make([]sql.RawBytes, len(columns))
+	scanArgs := make([]interface{}, len(values))
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+
+	for rows.Next() {
+		err = rows.Scan(scanArgs...)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		var value string
+		for i, col := range values {
+			if col == nil {
+				value = "NULL"
+			} else {
+				value = string(col)
+			}
+			fmt.Println(columns[i], ": ", value)
+		}
+		fmt.Println("-----------------------------------")
+	}
+	if err = rows.Err(); err != nil {
+		panic(err.Error())
+	}
+}

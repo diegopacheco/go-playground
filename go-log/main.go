cac747c07076034b14460741f24609134bfe1267
Diego Pacheco <diego.pacheco.it@gmail.com>
2019-09-23 13:41:06 -0300

    fun with go

--- go-log/main.go
+++ go-log/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+  "os"
 	"github.com/phuslu/log"
 )
 
 func main() {
 	log.Debug().Str("foo", "bar").Msg("hello world")
+
+  if log.IsTerminal(os.Stderr.Fd()) {
+		log.DefaultLogger = log.Logger{
+			Caller: 1,
+			Writer: &log.ConsoleWriter{ANSIColor: true},
+		}
+	}
+	log.Info().Str("foo", "bar").Msg("hello world")
 }
 
 // Output: {"time":"2019-07-10T16:00:19.092Z","level":"debug","foo":"bar","message":"hello world"}

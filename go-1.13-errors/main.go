158eb4d3510a20aacaa692cd278bfcb4cced0583
Diego Pacheco <diego.pacheco.it@gmail.com>
2019-11-27 01:12:03 -0800

    go 1.13 error handler fun

--- go-1.13-errors/main.go
+++ go-1.13-errors/main.go
@@ -9,10 +9,17 @@ func main() {
 
 	var ErrNotFound = errors.New("not found")
 	var e = fmt.Errorf("%q: %w", "I could not foudn the error - LOL", ErrNotFound)
-	if e != nil {
+	if errors.Is(e, ErrNotFound) {
 		println("e is ErrNotFound")
 		println(e)
 		println(e.Error())
 	}
 
+	type MyError struct {
+		Env   string
+		Blame string
+		Err   error
+	}
+	var e2 = MyError{Env: "production", Blame: "No One", Err: errors.New("MyError jush happend")}
+	println("Env: " + e2.Env + " blame: " + e2.Blame + " err: " + e2.Err.Error())
 }

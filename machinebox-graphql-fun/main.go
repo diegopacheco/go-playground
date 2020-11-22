837a8e5da9a8af9f3782e68b964e94f877927cc4
diegopacheco <diego.pacheco.it@gmail.com>
2020-11-22 19:07:20 -0300

    graphql go client working

---
+++ machinebox-graphql-fun/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/machinebox/graphql"
+)
+
+func main() {
+
+	client := graphql.NewClient("http://localhost:8080/graphql")
+
+	// make a request
+	req := graphql.NewRequest(`
+	query ($key:ID!){
+		bookById (id:$key) {
+			name,
+			pageCount,
+			author {
+			  id,
+			  firstName,
+			  lastName
+			}
+		}	
+    }
+	`)
+
+	// set any variables
+	req.Var("key", "book-1")
+
+	// set header fields
+	req.Header.Set("Cache-Control", "no-cache")
+
+	// define a Context for the request
+	ctx := context.Background()
+
+	// run it and capture the response
+	var respData map[string]interface{}
+	if err := client.Run(ctx, req, &respData); err != nil {
+		log.Fatal(err)
+	}
+
+	b, err := json.MarshalIndent(respData, "", "  ")
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	fmt.Print(string(b))
+}

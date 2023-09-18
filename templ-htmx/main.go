fe284f6b2acd1e6563900aa5a670016c20332f80
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-17 19:44:51 -0700

    hy lang - collections

--- templ-htmx/main.go
+++ templ-htmx/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
-	"context"
-	"os"
+	"fmt"
+	"net/http"
 
+	"github.com/a-h/templ"
 	"github.com/a-h/templ-examples/hello-world/src/components"
 )
 
 func main() {
-	component := components.Page()
-	component.Render(context.Background(), os.Stdout)
+	component := components.Page(1, 1)
+	http.Handle("/", templ.Handler(component))
+
+	fmt.Println("Listening on http://127.0.0.1:8080")
+	http.ListenAndServe(":8080", nil)
 }

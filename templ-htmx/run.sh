a35199d88c8befed9da72428def866b38b7491b5
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-17 20:37:27 -0700

    go lang - Templ - htmx

--- templ-htmx/run.sh
+++ templ-htmx/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+templ generate
 go run *.go
--- templ-htmx/src/components/components_templ.go
+++ templ-htmx/src/components/components_templ.go
@@ -160,7 +160,7 @@ func Page(global, session int) templ.Component {
 		if err != nil {
 			return err
 		}
-		_, err = templBuffer.WriteString("</title><link rel=\"stylesheet\" href=\"/assets/css/bulma.min.css\"><link rel=\"icon\" type=\"image/png\" sizes=\"48x48\" href=\"/assets/favicon/favicon.ico\"><link rel=\"manifest\" href=\"/assets/favicon/site.webmanifest\"><script src=\"/assets/js/htmx.min.js\">")
+		_, err = templBuffer.WriteString("</title><link rel=\"stylesheet\" href=\"/assets/css/bulma.min.css\"><link rel=\"icon\" type=\"image/png\" sizes=\"48x48\" href=\"/assets/favicon/favicon.ico\"><link rel=\"manifest\" href=\"/assets/favicon/manifest.json\"><script src=\"/assets/js/htmx.min.js\">")
 		if err != nil {
 			return err
 		}

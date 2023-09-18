19908f1dc526a014d246e15f37e574121fb62497
diegopacheco <diego.pacheco.it@gmail.com>
2023-09-17 20:33:21 -0700

    go lang - Templ - htmx

--- templ-htmx/README.md
+++ templ-htmx/README.md
@@ -11,6 +11,20 @@ following htmx sample from here
 * Removed dynamod DB from AWS
 * Added a in-memory implementation in Go with a hashmap
 
+### Diego Tradeoffs analysis
+
+## PROS
+
+* Lightling Fast
+* Relatively Simple
+* Leverages the backend
+
+## Cons
+
+* Templ code generation, everytime changes .templ file need to re-generate ```templ generate```
+* There is temporal coupling with the "js framework" with allow you to not "write as much js"
+* There is a risk of building a distributed monolith
+
 ## App running
 ```bash
 ./run.sh
--- templ-htmx/src/components/components_templ.go
+++ templ-htmx/src/components/components_templ.go
@@ -160,7 +160,7 @@ func Page(global, session int) templ.Component {
 		if err != nil {
 			return err
 		}
-		_, err = templBuffer.WriteString("</title><link rel=\"stylesheet\" href=\"/assets/css/bulma.min.css\"><link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"/assets/favicon/apple-touch-icon.png\"><link rel=\"icon\" type=\"image/png\" sizes=\"32x32\" href=\"/assets/favicon/favicon-32x32.png\"><link rel=\"icon\" type=\"image/png\" sizes=\"16x16\" href=\"/assets/favicon/favicon-16x16.png\"><link rel=\"manifest\" href=\"/assets/favicon/site.webmanifest\"><script src=\"/assets/js/htmx.min.js\">")
+		_, err = templBuffer.WriteString("</title><link rel=\"stylesheet\" href=\"/assets/css/bulma.min.css\"><link rel=\"icon\" type=\"image/png\" sizes=\"48x48\" href=\"/assets/favicon/favicon.ico\"><link rel=\"manifest\" href=\"/assets/favicon/site.webmanifest\"><script src=\"/assets/js/htmx.min.js\">")
 		if err != nil {
 			return err
 		}

f21bbe5a5db0a7bfc245fd19e9230af484d371e1
diegopacheco <diego.pacheco.it@gmail.com>
2024-01-09 00:22:16 -0800

    awesome progress bar for console in golang

---
+++ mpb-fun/go.mod
@@ -0,0 +1,13 @@
+module github.com/diegopacheco/mpb-fun
+
+go 1.21.0
+
+require github.com/vbauerster/mpb/v8 v8.7.2
+
+require (
+	github.com/VividCortex/ewma v1.2.0 // indirect
+	github.com/acarl005/stripansi v0.0.0-20180116102854-5a71ef0e047d // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+)
---
+++ mpb-fun/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/vbauerster/mpb/v8"
+	"github.com/vbauerster/mpb/v8/decor"
+)
+
+func main() {
+	// initialize progress container, with custom width
+	p := mpb.New(mpb.WithWidth(64))
+
+	total := 100
+	name := "Single Bar:"
+	// create a single bar, which will inherit container's width
+	bar := p.New(int64(total),
+		// BarFillerBuilder with custom style
+		mpb.BarStyle().Lbound("╢").Filler("▌").Tip("▌").Padding("░").Rbound("╟"),
+		mpb.PrependDecorators(
+			// display our name with one space on the right
+			decor.Name(name, decor.WC{C: decor.DindentRight | decor.DextraSpace}),
+			// replace ETA decorator with "done" message, OnComplete event
+			decor.OnComplete(decor.AverageETA(decor.ET_STYLE_GO), "done"),
+		),
+		mpb.AppendDecorators(decor.Percentage()),
+	)
+	// simulating some work
+	max := 100 * time.Millisecond
+	for i := 0; i < total; i++ {
+		time.Sleep(time.Duration(rand.Intn(10)+1) * max / 10)
+		bar.Increment()
+	}
+	// wait for our bar to complete and flush
+	p.Wait()
+}
